6c9b3b40c86e10e7489d4b29cedb1aee40cfbdcb
Sanjay Singh Rawat <sanjay@drake.co.in>
2025-07-31 13:39:35 +0530

    App Form Ui done

--- src/app/pages/apps/components/app-form/app-form.component.ts
+++ src/app/pages/apps/components/app-form/app-form.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './app-form.component.scss'
 })
 export class AppFormComponent {
+onClose() {
+throw new Error('Method not implemented.');
+}
 
 }
